efee6da9b94a752e75963fc0fdca25454c9b1694
Roy Lee <roylee@ucsb.edu>
2026-04-08 18:12:11 -0700

    rl - finished team tests

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -23,4 +23,82 @@ public class TeamTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
+
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+
+    @Test
+    public void equals_returns_true_for_same_team() {
+        assertEquals(true, team.equals(team));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_class() {
+        Object obj = new Object();
+        assertEquals(false, team.equals(obj));
+    }
+
+    @Test
+    public void equals_returns_true_for_same_fields() {
+        Team team2 = new Team(team.getName());
+        assertEquals(true, team.equals(team2));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_members() {
+        Team team2 = new Team(team.getName());
+        team2.addMember("different-member");
+        assertEquals(false, team.equals(team2));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_name() {
+        Team team2 = new Team("different-team");
+        assertEquals(false, team.equals(team2));
+    }
+
+    @Test
+    public void equals_returns_false_for_different_name_and_members() {
+        Team team2 = new Team("different-team");
+        team2.addMember("different-member");
+        assertEquals(false, team.equals(team2));
+    }
+
+    @Test
+    public void hashCode_returns_same_hash_for_same_fields() {
+        Team team2 = new Team(team.getName());
+        team2.setMembers(team.getMembers());
+        assertEquals(team.hashCode(), team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_returns_different_hash_for_different_fields() {
+        Team team2 = new Team("different-team");
+        team2.addMember("different-member");
+        assertEquals(false, team.hashCode() == team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_returns_different_hash_for_different_name() {
+        Team team2 = new Team("different-team");
+        assertEquals(false, team.hashCode() == team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_returns_different_hash_for_different_members() {
+        Team team2 = new Team(team.getName());
+        team2.addMember("different-member");
+        assertEquals(false, team.hashCode() == team2.hashCode());
+    }
+
+    @Test
+    public void hashCode_special_test() {
+        Team t = new Team();
+        int result = t.hashCode();
+        int expectedResult = 1;
+        assertEquals(expectedResult, result);
+    }
 }
